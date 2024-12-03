e0dab4157ceba6708cfd72cf7ddc8093f03f89a4
Thea <theola51924@stud.noroff.no>
2024-12-03 19:42:48 +0700

    configured tailwind with a theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      DEFAULT: "#19161A",
+      black: "#19161A",
+      white: "#F8F5F0",
+      realWhite: "#FFFFFF",
+      background: "#F0E4D4",
+      lazz: "#E3E3EE",
+      holidaze: "#C0D1B9",
+      bazaar: "#FFEAE6",
+      cta: {
+        ctaPrimary: "#5163EF",
+        ctaSecondary: "#E0AB7F",
+      },
+      contrast: {
+        contrastPrimary: "#83876A",
+        contrastSecondary: "#D5D3C4",
+      },
+    },
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    function ({ addBase, theme }) {
+      function extractColorVars(colorObj, colorGroup = "") {
+        return Object.keys(colorObj).reduce((vars, colorKey) => {
+          const value = colorObj[colorKey];
 
+          const newVars = typeof value === "string" ? { [`--color${colorGroup}-${colorKey}`]: value } : extractColorVars(value, `-${colorKey}`);
+
+          return { ...vars, ...newVars };
+        }, {});
+      }
+
+      addBase({
+        ":root": extractColorVars(theme("colors")),
+      });
+    },
+  ],
+};
