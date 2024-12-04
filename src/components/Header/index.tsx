a099d67acca5366e4cfbc27d619b16d290936442
Thea <theola51924@stud.noroff.no>
2024-12-04 22:50:10 +0700

    removed old code

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,8 +4,6 @@ export default function Header() {
       <div className="absolute inset-0 flex flex-col justify-center items-center">
         <div className="text-center flex flex-col justify-center items-center gap-5 mb-20">
           <h2 className=" font-libre-baskerville text-4xl">I make professional websites and web-applications</h2>
-          {/* 
-          <h1 className="font-meow-script-regular xl:text-9xl">I'm Thea</h1> */}
         </div>
         <button className="hover:scale-105 bg-cta-ctaPrimary2 bg-opacity-100 hover:bg-opacity-80 font-nunito-sans rounded-full text-xl text-white px-12 py-5 transition ease-in-out duration-300 hover:shadow-md">work with me</button>
       </div>
