0c4773288cf418485dca9e2c00d19789854801e7
Thea <theola51924@stud.noroff.no>
2024-12-04 14:21:05 +0700

    updated the Header design

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,11 +2,12 @@ export default function Header() {
   return (
     <header className="relative w-full h-screen backgroundImage border-b">
       <div className="absolute inset-0 flex flex-col justify-center items-center">
-        <div className="text-center flex flex-col justify-center items-center gap-5 mb-16">
-          <h1 className="font-meow-script-regular xl:text-9xl">I'm Thea</h1>
-          <h2 className="font-nunito-sans text-2xl">I make professional websites and web-applications</h2>
+        <div className="text-center flex flex-col justify-center items-center gap-5 mb-20">
+          <h2 className=" font-libre-baskerville text-4xl">I make professional websites and web-applications</h2>
+{/* 
+          <h1 className="font-meow-script-regular xl:text-9xl">I'm Thea</h1> */}
         </div>
-        <button className="hover:scale-105 bg-cta-ctaPrimary bg-opacity-50 hover:bg-cta-ctaSecondary font-libre-baskerville text-xl border rounded-full px-10 py-4 transition ease-in-out duration-300 hover:shadow-md">work with me</button>
+        <button className="hover:scale-105 bg-cta-ctaPrimary2 bg-opacity-100 hover:bg-opacity-80 font-nunito-sans rounded-full text-xl text-white px-12 py-5 transition ease-in-out duration-300 hover:shadow-md">work with me</button>
       </div>
     </header>
   );
