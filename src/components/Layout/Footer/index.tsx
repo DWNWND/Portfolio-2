95f4a34cf508528e9da1c8fc6677ae0bdf6a33f2
Thea <theola51924@stud.noroff.no>
2024-12-06 15:51:22 +0700

    added Footer

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -1,7 +1,33 @@
 export default function Footer() {
   return (
-    <footer>
-      <p>Footer</p>
+    <footer className="bg-lazz p-20 grid grid-cols-3 border-t">
+      <div className="flex gap-10">
+        <div className="flex flex-col gap-8 font-nunito-sans ">
+          <div>
+            <h3 className="text-xl font-bold uppercase text-nowrap">Inquiries can be sent to:</h3>
+            <p>xxx@email.com</p>
+          </div>
+          <div>
+            <h3 className=" text-xl font-bold uppercase">Address:</h3>
+            <p>Street Name</p>
+            <p>00000, City</p>
+            <p>Country</p>
+          </div>
+        </div>
+      </div>
+      <CopyRight />
+      <div className="flex flex-col items-end">
+        <div>LOGO</div>
+      </div>
     </footer>
+  );
+}
+
+function CopyRight(): JSX.Element {
+  return (
+    <div className="py-1 mt-12 w-full flex  flex-col justify-center items-center text-primary-blue">
+      <p>&copy; 2024 Thea Oland</p>
+      <p>org.nr.0000.000.0000</p>
+    </div>
   );
 }
