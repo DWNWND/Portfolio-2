261d6d3de086a1814f2a50b1905881ed81a40cfd
Thea <theola51924@stud.noroff.no>
2024-12-19 22:32:22 +0700

    styled for mobile

--- src/components/ServicesAccordion/index.tsx
+++ src/components/ServicesAccordion/index.tsx
@@ -22,23 +22,22 @@ interface ServicesAccordionProps {
 
 export default function ServicesAccordion({ toggleAccordion, openAccordion }: ServicesAccordionProps): JSX.Element {
   const isOpen = openAccordion === 40;
-  const accordionBoxStyle = `${isOpen ? "bg-background" : "bg-beige"} font-nunito-sans text-black py-6 px-8 rounded-lg h-full border shadow-md`;
+  const accordionBoxStyle = `${isOpen ? "bg-background" : "bg-beige"} font-nunito-sans text-black py-6 px-6 rounded-lg h-full border shadow-md`;
 
   return (
     <div className="px-4 md:px-0">
       <div className="flex flex-col gap-4 max-w-[64rem] m-auto mb-16">
         <div className="px-4 xl:max-w-screen-xl m-auto md:w-[64rem] max-w-[64rem]">
-          <h2 className="font-nunito-sans text-xl uppercase font-bold mb-3 text-center">my services:</h2>
+          <h2 className="font-nunito-sans text-xl uppercase font-bold mb-4 md:mb-6 text-center">my services:</h2>
         </div>
         {servicesArr.map((service) => {
           return <ServiceAccordionBox service={service} isOpen={openAccordion === service.id} toggle={() => toggleAccordion(service.id)} key={service.id} />;
         })}
         <div className={accordionBoxStyle}>
           <div className="flex items-center gap-2 justify-between cursor-pointer" onClick={() => toggleAccordion(40)}>
-            <div className="flex items-center gap-8 text-2xl">
-              <BiGroup />
-
-              <h2 className="text-2xl" id="40">
+            <div className="flex items-center gap-8">
+              <BiGroup className="text-6xl md:text-2xl"/>
+              <h2 className="text-lg md:text-2xl" id="40">
                 Front-End Development for Startups & Bigger Projects
               </h2>
             </div>
@@ -48,21 +47,21 @@ export default function ServicesAccordion({ toggleAccordion, openAccordion }: Se
               </span>
             </div>
           </div>
-          <div className={`px-[2.5rem]  flex flex-col gap-8 transition-max-height duration-500 ease-in-out overflow-hidden ${isOpen ? "max-h-[1500px] opacity-100 mt-4 mb-4 pt-12" : "max-h-0 opacity-0"}`}>
-            <h2 className="text-center text-xl font-bold">Need a Front-End Developer for Your App, Startup, or Growing Team?</h2>
-            <p className="px-8 text-center text-lg">I help startups and businesses bring their ideas to life with modern, responsive, and user-friendly front-end solutions. Whether you’re building a new product, scaling an existing app, or looking for a front-end developer to join your team, I’m ready to jump in and collaborate.</p>
+          <div className={`md:px-[2.5rem] flex flex-col gap-8 transition-max-height duration-500 ease-in-out overflow-hidden ${isOpen ? "max-h-[2500px] opacity-100 mt-4 mb-4 pt-12" : "max-h-0 opacity-0"}`}>
+            <h2 className="md:text-center text-xl font-bold">Need a Front-End Developer for Your App, Startup, or Growing Team?</h2>
+            <p className=" md:text-center text-lg">I help startups and businesses bring their ideas to life with modern, responsive, and user-friendly front-end solutions. Whether you’re building a new product, scaling an existing app, or looking for a front-end developer to join your team, I’m ready to jump in and collaborate.</p>
             <p className="text-2xl leading-relaxed text-center font-libre-baskerville">Send me a message, and let’s discuss your needs, timeline, and vision.</p>
-            <div className="w-[15rem] mx-auto mt-8 mb-10">
+            <div className="w-[15rem] mx-auto md:mt-8 mb-5">
               <div className="relative group">
-                <a href="#contact" className={`bg-cta-primaryBright text-center block hover:scale-105  bg-opacity-100 z-[20] relative font-nunito-sans rounded-full text-2xl text-white px-12 py-5 transition-all ease-in-out duration-300 group-hover:shadow-lg group-hover:bg-gradient-to-r from-blue-500 to-purple-600`}>
+                <a href="#contact" className={`bg-cta-primaryBright text-center block hover:scale-105  bg-opacity-100 z-[20] relative font-nunito-sans rounded-full text-lg md:text-2xl text-white px-12 py-5 transition-all ease-in-out duration-300 group-hover:shadow-lg group-hover:bg-gradient-to-r from-blue-500 to-purple-600`}>
                   <p className="tracking-wider font-bold">let's talk</p>
                 </a>
                 <div className="rounded-full w-full h-full top-2 left-2 absolute bg-black z-[10] transition-all duration-300 ease-in-out group-hover:top-0 group-hover:left-0 group-hover:opacity-50"></div>
               </div>
             </div>
-            <div className="p-8 rounded-lg bg-white">
+            <div className="p-6 md:p-8 rounded-lg bg-white">
               <h2 className="text-xl font-bold">What I Bring to the Table</h2>
-              <ul className="flex flex-col gap-4 mt-6 text-lg">
+              <ul className="flex flex-col gap-6 mt-6 md:gap-4 text-lg">
                 <li>
                   <div className="flex gap-2 items-center">
                     <GoDotFill />
@@ -137,7 +136,7 @@ function ServiceAccordionBox({ service, isOpen, toggle }: { service: Service; is
           {service.name.toLowerCase().includes("development") && <CgIfDesign />}
           {service.name.toLowerCase().includes("design") && <FaRegCopyright />}
           {service.name.toLowerCase().includes("launch") && <HiOutlineRocketLaunch />}
-          <h2 className="text-2xl" id={JSON.stringify(service.id)}>
+          <h2 className="text-lg md:text-2xl" id={JSON.stringify(service.id)}>
             {service.name}
           </h2>
         </div>
@@ -148,19 +147,21 @@ function ServiceAccordionBox({ service, isOpen, toggle }: { service: Service; is
         </div>
       </div>
       <div className={` transition-max-height duration-500 ease-in-out overflow-hidden ${isOpen ? "max-h-[1000px] opacity-100 mt-4 mb-4" : "max-h-0 opacity-0"}`}>
-        <p className="text-sm px-[3.5rem]">All mentioned prices are subject to 25% VAT</p>
-        <div className="rounded-lg text-lg px-[3.5rem] py-12">
+        <p className="text-sm md:px-[3.5rem]">All mentioned prices are subject to 25% VAT</p>
+        <div className="rounded-lg text-lg md:px-[3.5rem] py-12">
           {service.cta && <p className="font-nunito-sans mb-8">{service.cta}</p>}
           <ul className="text-lg flex flex-col gap-4 font-nunito-sans">
             {service.includes.map((including) => {
               return (
-                <li className="flex justify-between border-b pb-4" key={including.id}>
-                  <div className="flex gap-4 items-center">
-                    <GoDotFill />
-                    <h3 className="font-bold">{including.name}</h3>
-                    {including.info && <p className="italic">({including.info})</p>}
+                <li className="flex flex-col  md:flex-row justify-between gap-4 border-b pb-6 md:pb-4" key={including.id}>
+                  <div className="flex flex-col">
+                    <div className="flex gap-4 items-center">
+                      <GoDotFill />
+                      <h3 className="font-bold ">{including.name}</h3>
+                    </div>
+                    {including.info && <p className="italic ml-8">({including.info})</p>}
                   </div>
-                  {including.price && <div className="font-bold text-black text-center rounded-lg">{including.price}</div>}
+                  {including.price && <div className="font-bold text-black md:text-center rounded-lg ml-8">{including.price}</div>}
                 </li>
               );
             })}
@@ -170,7 +171,7 @@ function ServiceAccordionBox({ service, isOpen, toggle }: { service: Service; is
         </div>
         <div className="flex justify-center mb-4">
           <div className="relative group">
-            <a href="#packages" className="tracking-wider block font-bold text-center hover:scale-105 bg-white border border-black bg-opacity-100 z-[20] relative font-nunito-sans rounded-full text-2xl text-black hover:text-opacity-90 px-12 py-5 transition-all ease-in-out duration-300 group-hover:shadow-lg group-hover:bg-gradient-to-r from-blue-500 to-purple-600">
+            <a href="#packages" className="tracking-wider block font-bold text-center hover:scale-105 bg-white border border-black bg-opacity-100 z-[20] relative font-nunito-sans rounded-full text-lg md:text-2xl text-black hover:text-opacity-90 px-12 py-5 transition-all ease-in-out duration-300 group-hover:shadow-lg group-hover:bg-gradient-to-r from-blue-500 to-purple-600">
               see my packages
             </a>
             <div className="rounded-full w-full h-full top-2 left-2 absolute bg-black z-[10] transition-all duration-300 ease-in-out group-hover:top-0 group-hover:left-0 group-hover:opacity-50"></div>
--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -46,21 +46,22 @@ export default function Home() {
       <div className="bg-white relative">
         {imageModalOpen && <ImageModal image={currentImage} alt={currentAlt} toggle={closeModal} />}
         <section id="home">
-          <header className="relative w-full h-screen backgroundImage border-b">
-            <div className="absolute inset-0 flex flex-col justify-center items-center">
-              {isMobile && <h1 className="font-meow-script-regular text-center text-6xl mb-10 ">I'm Thea</h1>}
-              <div className="text-center flex flex-col justify-center items-center gap-5 md:mb-10 linearWhiteBackground p-20">
-                <h2 className=" font-libre-baskerville text-4xl ">I make professional websites and web-applications for a living</h2>
+          <header className="w-full h-screen backgroundImage border-b p-4 flex justify-center items-center">
+            <div className="flex flex-col gap-10 md:gap-20 justify-center items-center">
+              {isMobile && <h1 className="font-meow-script-regular relative z-50 text-center text-7xl">devThea</h1>}
+              <div className="relative">
+                <h2 className="font-libre-baskerville relative text-center z-50 text-2xl md:text-4xl">I make professional websites and web-applications for a living.</h2>
+                <div className="absolute w-full bottom-[-130px] linearWhiteBackground py-40 md:p-40"></div>
               </div>
               <div className="relative group">
-                <a href="#packages" className="tracking-wider block font-bold text-center hover:scale-105 bg-white border border-black bg-opacity-100 z-[20] relative font-nunito-sans rounded-full text-2xl text-black hover:text-opacity-90 px-12 py-5 transition-all ease-in-out duration-300 group-hover:shadow-lg group-hover:bg-gradient-to-r from-blue-500 to-purple-600">
+                <a href="#packages" className="tracking-wider block font-bold text-center hover:scale-105 bg-white border border-black bg-opacity-100 z-[20] relative font-nunito-sans rounded-full text-lg md:text-2xl text-black hover:text-opacity-90 px-12 py-5 transition-all ease-in-out duration-300 group-hover:shadow-lg group-hover:bg-gradient-to-r from-blue-500 to-purple-600">
                   check pricing packages
                 </a>
                 <div className="rounded-full w-full h-full top-2 left-2 absolute bg-black z-[10] transition-all duration-300 ease-in-out group-hover:top-0 group-hover:left-0 group-hover:opacity-50"></div>
               </div>
             </div>
           </header>
-          <div className="px-4 pt-60 pb-32 xl:max-w-screen-xl m-auto">
+          <div className="px-4 pt-28 md:pt-60 pb-32 xl:max-w-screen-xl m-auto">
             <h1 className="font-libre-baskerville text-2xl md:text-6xl leading-relaxed text-center">
               I turn <em>complex functionality</em> into
               <b>
@@ -72,24 +73,24 @@ export default function Home() {
           </div>
         </section>
 
-        <section id="services" className="min-h-[36rem] md:py-[120px] ">
+        <section id="services" className="min-h-[36rem] md:py-[120px] pt-24 ">
           <div className="m-auto">
             <ServicesAccordion toggleAccordion={toggleAccordion} openAccordion={openAccordion} />
           </div>
         </section>
-        <section id="packages" className="min-h-[36rem] border-b">
+        <section id="packages" className="min-h-[36rem] border-b pt-24 md:pt-0">
           <div className="m-auto">
-            <div className="border-b">
-              <h2 className="text-center px-4 xl:max-w-screen-xl m-auto md:w-[64rem] max-w-[64rem] font-nunito-sans text-xl uppercase font-bold mb-6">my packages:</h2>
+            <div className="md:border-b">
+              <h2 className="text-center px-4 xl:max-w-screen-xl m-auto md:w-[64rem] max-w-[64rem] font-nunito-sans text-xl uppercase font-bold mb-4 md:mb-6">my packages:</h2>
             </div>
-            <div className="backgroundImage pt-40">
-              <div className="xl:max-w-screen-xl m-auto md:grid md:grid-cols-3 gap-10">
+            <div className="backgroundImage md:pt-40">
+              <div className="xl:max-w-screen-xl m-auto grid md:grid-cols-3 gap-4 md:gap-10 p-4">
                 {packagesArr.map((packages) => {
                   return <PackageCards packages={packages} key={packages.id} toggleAccordion={toggleAccordion} />;
                 })}
               </div>
               <div className="linearBlackBackground text-black py-40">
-                <div className="bg-white border-y shadow-md p-12 ">
+                <div className="bg-white border-y shadow-md py-12 px-6 md:p-12 ">
                   <h1 className="text-2xl md:text-4xl leading-relaxed text-center mb-8  font-libre-baskerville font-bold">Not sure where to start?</h1>{" "}
                   <h1 className="text-2xl md:text-2xl font-nunito-sans leading-relaxed text-center">
                     <a href="#contact" className="relative inline gap-2 font-nunito-sans text-blue-500 text-center group">
@@ -104,21 +105,21 @@ export default function Home() {
           </div>
         </section>
 
-        <section id="about" className="bg-white min-h-[40rem] xl:max-w-screen-xl m-auto">
-          <div className="flex flex-col md:flex-row justify-between items-center py-6 md:py-32 gap-16">
+        <section id="about" className="bg-white min-h-[40rem] xl:max-w-screen-xl m-auto pt-24 md:pt-0">
+          <h2 className="md:hidden text-center px-4 xl:max-w-screen-xl m-auto md:w-[64rem] max-w-[64rem] font-nunito-sans text-xl uppercase font-bold mb-4 md:mb-6">about me:</h2>
+          <div className="flex flex-col md:flex-row justify-between items-center pt-8 pb-12 md:py-32 gap-16">
             <div className="relative mb-[65px]">
-              <img className="object-cover rounded-full h-[16rem] min-w-[16rem] md:h-[30rem] md:min-w-[30rem] border" src={profileImage} alt="I’m Thea—a passionate front-end developer and web designer dedicated to crafting user-friendly websites and impactful digital experiences."></img>
-              <h2 className="font-meow-script-regular text-6xl xl:text-9xl absolute bottom-[-65px] linearWhiteBackground  inset-x-0 text-center">I’m Thea</h2>
+              <img className="object-cover rounded-full h-[20rem] min-w-[20rem] md:h-[30rem] md:min-w-[30rem] border" src={profileImage} alt="I’m Thea—a passionate front-end developer and web designer dedicated to crafting user-friendly websites and impactful digital experiences."></img>
+              <h2 className="font-meow-script-regular text-7xl xl:text-9xl absolute bottom-[-100px] md:bottom-[-65px] linearWhiteBackground inset-x-0 text-center">I’m Thea</h2>
             </div>
-
-            <div>
-              <div className="text-lg leading-relaxed">
-                <p className="mb-4 font-nunito-sans">I’ve always loved turning ideas into something real while combining design and practicality.</p>
-                <p className="mb-4 font-nunito-sans">From building dollhouses in my father’s workshop as a child to solving complex challenges as an adult, my passion for design and problem-solving has been a constant. Two years ago I decided to the pursue my love for design and technology. While sailing on a boat in the Mediterranean with my husband I re-educated myself as a front-end developer, graduating with top grades.</p>
-                <p className="mb-4 font-nunito-sans"> As a front-end developer and web designer, I spend my days crafting websites and applications that are not only functional but also enjoyable to use. My work is all about creating a balance between brand designs and smooth functionality, I find joy in solving puzzles and making things look just right.</p>
-                <p className="font-nunito-sans "> When I’m not coding or tweaking layouts, you’ll probably find me sketching new home improvement ideas, experimenting with new tech, or sipping coffee while dreaming up my next big idea.</p>
+            <div className="px-8 md:p-0">
+              <div className="text-lg leading-relaxed flex flex-col gap-4 font-nunito-sans">
+                <p>I’ve always loved turning ideas into something real while combining design and practicality.</p>
+                <p>From building dollhouses in my father’s workshop as a child to solving complex challenges as an adult, my passion for design and problem-solving has been a constant. Two years ago I decided to the pursue my love for design and technology. While sailing on a boat in the Mediterranean with my husband I re-educated myself as a front-end developer, graduating with top grades.</p>
+                <p> As a front-end developer and web designer, I spend my days crafting websites and applications that are not only functional but also enjoyable to use. My work is all about creating a balance between brand designs and smooth functionality, I find joy in solving puzzles and making things look just right.</p>
+                <p> When I’m not coding or tweaking layouts, you’ll probably find me sketching new home improvement ideas, experimenting with new tech, or sipping coffee while dreaming up my next big idea.</p>
               </div>
-              <div className="flex gap-8 text-4xl mt-2 mr-2 justify-end">
+              <div className="flex gap-8 text-4xl mt-12 md:mt-2 mr-2 justify-center md:justify-end">
                 <Link to="https://www.linkedin.com/in/thea-oland-b38175139/">
                   <BsLinkedin className="hover:scale-110 transition-all duration-300 ease-in-out" />
                 </Link>
@@ -133,25 +134,24 @@ export default function Home() {
           </div>
         </section>
 
-        <section id="contact" className="min-h-screen border-t backgroundImage">
+        <section id="contact" className="min-h-screen border-t backgroundImage pt-24 md:pt-0">
           <div className="m-auto max-w-[48rem] p-4">
             <ContactForm />
           </div>
         </section>
-        <section id="portfolio" className=" min-h-[36rem] border-t">
-          <h2 className="text-center px-4 m-auto w-full font-nunito-sans text-xl uppercase font-bold pb-6 sr-only">portfolio:</h2>
-
+        <section id="portfolio" className=" min-h-[36rem] md:border-t pt-24 md:pt-0">
+          <h2 className="md:hidden text-center px-4 xl:max-w-screen-xl m-auto md:w-[64rem] max-w-[64rem] font-nunito-sans text-xl uppercase font-bold mb-4 md:mb-6">my portfolio:</h2>
           <div className="m-auto">
-            <div className="flex w-full border-b">
-              <button onClick={() => setFilter("academic")} className={`${buttonClass} ${filter === "academic" ? "bg-opacity-100 text-white " : "bg-opacity-0 hover:bg-black hover:text-white"} pl-16`}>
+            <div className="flex flex-col md:flex-row w-full border-b">
+              <button onClick={() => setFilter("academic")} className={`${buttonClass} ${filter === "academic" ? "bg-opacity-100 text-white " : "bg-opacity-0 hover:bg-black hover:text-white"} border-y md:border-none md:pl-16`}>
                 academic projects
               </button>
-              <div className="w-[4px] bg-black"></div>
-              <button onClick={() => setFilter("professional")} className={`${buttonClass} ${filter === "professional" ? "bg-opacity-100 text-white " : "bg-opacity-0 hover:bg-black hover:text-white"}`}>
+              {/* <div className="w-[4px] bg-black"></div> */}
+              <button onClick={() => setFilter("professional")} className={`${buttonClass} ${filter === "professional" ? "bg-opacity-100 text-white " : "bg-opacity-0 hover:bg-black hover:text-white"} border-b md:border-x md:border-b-0`}>
                 professional projects
               </button>
-              <div className="w-[4px] bg-black"></div>
-              <button onClick={() => setFilter("personal")} className={`${buttonClass} ${filter === "personal" ? "bg-opacity-100 text-white" : "bg-opacity-0 hover:bg-black hover:text-white"} pr-16`}>
+              {/* <div className="w-[4px] bg-black"></div> */}
+              <button onClick={() => setFilter("personal")} className={`${buttonClass} ${filter === "personal" ? "bg-opacity-100 text-white" : "bg-opacity-0 hover:bg-black hover:text-white"} md:pr-16`}>
                 personal projects
               </button>
             </div>
