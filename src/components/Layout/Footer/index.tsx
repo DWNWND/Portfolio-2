ba0543fe94eb6fe65adb7490169c678c47f4ac19
Thea <theola51924@stud.noroff.no>
2024-12-19 19:07:22 +0700

    fixed footer design

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -1,33 +1,37 @@
+import { BsLinkedin } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 export default function Footer() {
   return (
-    <footer className="bg-lazz p-4 md:p-20 grid md:grid-cols-3 border-t">
+    <footer className="p-4 md:px-20 md:p-10 md:pb-16 grid md:grid-cols-3 border-t">
       <div className="flex gap-10">
-        <div className="flex flex-col gap-8 font-nunito-sans ">
+        <div className="flex flex-col gap-8 font-nunito-sans items-center justify-center">
           <div>
             <h3 className="text-xl font-bold uppercase text-nowrap">Inquiries can be sent to:</h3>
-            <p>thea@webname.com</p>
-          </div>
-          <div>
-            <h3 className="text-xl font-bold uppercase">Address:</h3>
-            <p>Street Name</p>
-            <p>00000, City</p>
-            <p>Country</p>
+            <p>thea@devthea.com</p>
           </div>
         </div>
       </div>
-      <CopyRight />
-      <div className="flex flex-col md:items-end items-center justify-center">
-        <div className="font-meow-script-regular text-[8rem]">T</div>
+      <div className="flex flex-col items-center justify-center">
+        <div className="font-meow-script-regular text-[7rem]">devThea</div>
+        <div className="w-full flex flex-col text-primary-blue text-center">
+          <p>&copy; 2024 devThea</p>
+          <p>org.nr.0000.000.0000</p>
+        </div>
+      </div>
+      <div className="flex gap-8 text-4xl justify-end items-center">
+        <Link to="https://www.linkedin.com/in/thea-oland-b38175139/">
+          <BsLinkedin className="hover:scale-110 transition-all duration-300 ease-in-out" />
+        </Link>
+        <Link to="https://www.instagram.com/thea.oland/">
+          <BsInstagram className="hover:scale-110 transition-all duration-300 ease-in-out" />
+        </Link>
+        <Link to="https://github.com/DWNWND">
+          <FaGithub className="hover:scale-110 transition-all duration-300 ease-in-out" />
+        </Link>
       </div>
     </footer>
-  );
-}
-
-function CopyRight(): JSX.Element {
-  return (
-    <div className="py-1 mt-12 w-full flex flex-col mb-10 md:mb-0 justify-center md:items-center text-primary-blue">
-      <p>&copy; 2024 Thea</p>
-      <p>org.nr.0000.000.0000</p>
-    </div>
   );
 }
